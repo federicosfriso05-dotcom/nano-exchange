d9e8e6f26dd944d15eb7d6a2af26dbd4b7031446
Federico Sfriso <federicosfriso05@gmail.com>
2026-03-01 20:06:40 +0100

    refactor: adjust underscore convention and general formatting

--- include/nano_exchange/memory_pool.hpp
+++ include/nano_exchange/memory_pool.hpp
@@ -1,7 +1,8 @@
+#pragma once
 #include <vector>
 #include <cstddef>
-#include <functional>   //for std::less()
-#include <stdexcept>   //for std::invalid_argument
+#include <functional>   // for std::less()
+#include <stdexcept>   // for std::invalid_argument
 
 namespace nano_exchange
 {
@@ -11,7 +12,7 @@ class MemoryPool
     private:
 
     // Using a union to map the object and the free-list index to the same physical bytes
-    // - when the position in empty, holds the size_t index of the next free slot
+    // - when the position is empty, holds the size_t index of the next free slot
     // - when it's full, the size_t is overwritten by the T object
     union Slot
     {
@@ -33,9 +34,9 @@ class MemoryPool
             throw std::invalid_argument("Capacity must be greater than 0.");
 
         for(size_t i = 0; i < capacity - 1; i++)
-            pool_[i].nextFreeIndex_ = i + 1;
+            pool_[i].nextFreeIndex = i + 1;
 
-        pool_[capacity - 1].nextFreeIndex_ = capacity;  // capacity is the "full" signal
+        pool_[capacity - 1].nextFreeIndex = capacity;  // capacity is the "full" signal
     }
 
     T* allocate()
@@ -44,7 +45,7 @@ class MemoryPool
             return nullptr;
         
         size_t temp = nextFreeIndex_;
-        nextFreeIndex_ = pool_[temp].nextFreeIndex_;
+        nextFreeIndex_ = pool_[temp].nextFreeIndex;
         return &pool_[temp].obj;
     }
 
@@ -55,13 +56,13 @@ class MemoryPool
         
         Slot* slot_pointer = reinterpret_cast<Slot*>(pointer);
 
-        //Check valid pointer bounds. std::less() avoids UB when comparing pointers
-        if(std::less<Slot*>{}(slot_pointer, pool.data()) || std::greater_equal{}(slot_pointer, pool.data() + pool.size()))
+        // Check valid pointer bounds. std::less() avoids UB when comparing pointers
+        if(std::less<Slot*>{}(slot_pointer, pool_.data()) || std::greater_equal<Slot*>{}(slot_pointer, pool_.data() + pool_.size()))
             return;
 
-        size_t difference = slot_pointer - pool.data();
-        pool[difference].nextFreeIndex = nextFreeIndex;
-        nextFreeIndex = difference;
+        size_t difference = slot_pointer - pool_.data();
+        pool_[difference].nextFreeIndex = nextFreeIndex_;
+        nextFreeIndex_ = difference;
     }
 
     ~MemoryPool() = default;
